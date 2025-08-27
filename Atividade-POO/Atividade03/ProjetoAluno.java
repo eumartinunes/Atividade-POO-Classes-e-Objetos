6c6264bf90f4fd68964e05a1eed6bf3e6f840d16
eumartinunes <martin2006nunes09@gmail.com>
2025-08-26 22:34:15 -0300

    ProjetoAluno.java

--- Atividade-POO/Atividade03/ProjetoAluno.java
+++ Atividade-POO/Atividade03/ProjetoAluno.java
@@ -1,5 +1,19 @@
 /*3) Crie um projeto chamado ProjetoAluno e construa nele a classe Aluno, 
-contendo os atributos:*/
+contendo os atributos: 
+• nome (String)
+ • matricula (int)
+ • notaAv1 (double)
+ • notaAv2 (double)
+ A classe deve ter métodos para:
+ • Mostrar os dados do aluno.
+ • Calcular a média
+ • Verificar aprovação, de acordo com as regras da UNAMA.
+ Em seguida:
+ 1. Crie dois objetos da classe Aluno.
+ 2. Atualize a nota de um aluno.
+ 3. Mostre os dados atualizados.
+ 4. Chame o método para verificar se está aprovado.
+ 5. Utilize a função calcular média dentro de verificar aprovação para saber se o aluno passou.*/
 public class ProjetoAluno {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno();
@@ -43,4 +57,5 @@ public class ProjetoAluno {
         
     }
 }
+
 
